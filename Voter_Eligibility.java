4dd6b588ad63336ef1594b90b38c659ce9a9b3cd
connect.sayantanis <sayantanisaha376@gmail.com>
2025-01-15 22:20:18 +0530

    Update Voter_Eligibility.java

--- Voter_Eligibility.java
+++ Voter_Eligibility.java
@@ -1,27 +1,19 @@
 //Create a class to check where a student is elligible 2 vote in an election by checking age.
 
-
 class Student {
     private String name;
     private int age;
 
-    // Constructor
-	
-    public Student(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
     // Getter and Setter methods
     public String getName() {
-        return name;
+        return this.name;
     }
     public void setName(String name) {
         this.name = name;
     }
 
     public int getAge() {
-        return age;
+        return this.age;
     }
     public void setAge(int age) {
         this.age = age;
@@ -38,15 +30,20 @@ class Student {
         System.out.println("Age: " + age);
         System.out.println("Eligible to vote: " + (isEligibleToVote() ? "Yes" : "No"));
     }
-	
-	
-	public static void main(String[] args) {
+
+    public static void main(String[] args) {
         // Create a student object
-        Student student = new Student("Sayantani", 21);
+        Student student = new Student();
+        student.setName("Sayantani");
+        student.setAge(21);
 
         // Display student details and voting eligibility
         student.displayDetails();
     }
 }
+
+																								        this.age = age;
+																										    }
+
 
 
